8c291a65e1d67a1408bef1974ed018619642fc8b
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2020-12-05 14:36:51 -0300

    -Fix pipeline barrier shader stages flags

--- eagle/core/source/renderer/vulkan/VulkanCommandBuffer.cpp
+++ eagle/core/source/renderer/vulkan/VulkanCommandBuffer.cpp
@@ -147,8 +147,8 @@ void VulkanCommandBuffer::set_scissor(uint32_t w, uint32_t h, uint32_t x, uint32
     VK_CALL vkCmdSetScissor(m_commandBuffer, 0, 1, &scissor);
 }
 
-void VulkanCommandBuffer::pipeline_barrier(const Reference<Image> &image, ShaderStage srcStage,
-                                           ShaderStage dstStage) {
+void VulkanCommandBuffer::pipeline_barrier(const Reference<Image> &image, const std::vector<PipelineStage> &srcPipelineStages,
+                                           const std::vector<PipelineStage> &dstPipelineStages) {
     VkImageMemoryBarrier imageMemoryBarrier = {};
     imageMemoryBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
     // We won't be changing the layout of the image
@@ -162,8 +162,8 @@ void VulkanCommandBuffer::pipeline_barrier(const Reference<Image> &image, Shader
     imageMemoryBarrier.dstQueueFamilyIndex = 0;
     VK_CALL vkCmdPipelineBarrier(
             m_commandBuffer,
-            VulkanConverter::to_vk(srcStage),
-            VulkanConverter::to_vk(dstStage),
+            VulkanConverter::to_vk_flags<VkPipelineStageFlags>(srcPipelineStages),
+            VulkanConverter::to_vk_flags<VkPipelineStageFlags>(dstPipelineStages),
             0,
             0, nullptr,
             0, nullptr,
