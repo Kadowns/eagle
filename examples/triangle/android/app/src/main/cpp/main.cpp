2e8e5d89ef0a43164cf3c22e073b6ea9c9bf9602
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-02-21 18:14:45 -0300

    -Include try catch on android_main

--- examples/triangle/android/app/src/main/cpp/main.cpp
+++ examples/triangle/android/app/src/main/cpp/main.cpp
@@ -5,13 +5,21 @@
 #include <android_native_app_glue.h>
 #include <jni.h>
 #include <iostream>
+#include <android/log.h>
 
 #include <eagle/platform/android/AndroidApplication.h>
 #include <TriangleApp.h>
 
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "threaded_app", __VA_ARGS__))
+
 extern "C" {
 void android_main(struct android_app *pApp) {
-    Eagle::AndroidApplication application(pApp, new TriangleApp());
-    application.run();
+    try {
+        Eagle::AndroidApplication application(pApp, new TriangleApp());
+        application.run();
+    }
+    catch (std::exception& e){
+        LOGE("exception caught!");
+    }
 }
 }
