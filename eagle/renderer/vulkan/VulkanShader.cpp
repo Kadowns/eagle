6bb22e90110586f23c4e69138c5ff1883a4e8d65
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-02-21 18:14:32 -0300

    -Allows prebuilt spv shader format

--- eagle/renderer/vulkan/VulkanShader.cpp
+++ eagle/renderer/vulkan/VulkanShader.cpp
@@ -5,6 +5,7 @@
 #include <eagle/renderer/vulkan/VulkanShaderUtils.h>
 #include <eagle/Log.h>
 #include <eagle/renderer/vulkan/VulkanRenderPass.h>
+#include <eagle/FileSystem.h>
 
 EG_BEGIN
 
@@ -21,8 +22,16 @@ VulkanShader::VulkanShader(const ShaderCreateInfo &createInfo, const VulkanShade
         if (stage == ShaderStage::COMPUTE){
             throw std::runtime_error("Compute shaders are not allowed on a graphics shader!");
         }
-
-        m_shaderCodes.emplace(VulkanConverter::to_vk(stage), VulkanShaderCompiler::compile_glsl(path, stage));
+        //TODO - Create a better way to load different file formats
+        if (path.find(".spv") != std::string::npos){
+            std::vector<uint8_t> byteCode = std::move(FileSystem::instance()->read_bytes(path));
+            std::vector<uint32_t> intCode(byteCode.size() / sizeof(uint32_t));
+            std::memcpy(intCode.data(), byteCode.data(), byteCode.size());
+            m_shaderCodes.emplace(VulkanConverter::to_vk(stage), std::move(intCode));
+        }
+        else {
+            m_shaderCodes.emplace(VulkanConverter::to_vk(stage), VulkanShaderCompiler::compile_glsl(path, stage));
+        }
     }
 
     create_pipeline_layout();
