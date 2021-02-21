b9aed245680de3b2bbae30b2d7654269ae69fc0b
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-02-21 00:40:31 -0300

    -Remove unsued findGlfw cmake file

--- cmake/FindGLFW.cmake
+++
@@ -1,9 +0,0 @@
-if (GLFW_FOUND)
-    return()
-endif ()
-set(GLFW_STANDALONE OFF)
-set(GLFW_INSTALL OFF)
-set(GLFW_BUILD_DOCS OFF)
-add_subdirectory(${CMAKE_HOME_DIRECTORY}/external/glfw ${CMAKE_BINARY_DIR}/glfw)
-
-set(GLFW_FOUND True)
