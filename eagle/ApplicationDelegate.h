12867970b2a2b027787f3f3e7a613eb257fe6044
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-02-21 00:45:00 -0300

    -Add virtual destructor to ApplicationDelegate

--- eagle/ApplicationDelegate.h
+++ eagle/ApplicationDelegate.h
@@ -11,6 +11,7 @@ EG_BEGIN
 
 class ApplicationDelegate {
 public:
+    virtual ~ApplicationDelegate() = default;
     virtual void init() = 0;
     virtual void step() = 0;
     virtual void destroy() = 0;
