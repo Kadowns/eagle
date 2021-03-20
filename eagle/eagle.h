ce914c31cbd66fe10e3906a1c2fd771d981155df
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-03-20 16:34:49 -0300

    -Remove core.h and move includes to eagle.h

--- eagle/eagle.h
+++ eagle/eagle.h
@@ -5,8 +5,21 @@
 #ifndef EAGLE_EAGLE_H
 #define EAGLE_EAGLE_H
 
+#include <eagle/log.h>
+#include <eagle/layer.h>
+#include <eagle/input_layer.h>
+#include <eagle/input.h>
+#include <eagle/random.h>
+#include <eagle/time.h>
+#include <eagle/application_delegate.h>
+#include <eagle/events/event.h>
+#include <eagle/events/input_events.h>
+#include <eagle/events/window_events.h>
+#include <eagle/events/key_codes.h>
 
-#include <eagle/core.h>
+#include <eagle/renderer/renderer_global_definitions.h>
+#include <eagle/renderer/rendering_context.h>
 
+#include <eagle/memory/stack_allocator.h>
 
 #endif //EAGLE_EAGLE_H
