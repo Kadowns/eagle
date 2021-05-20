e2fc4cefd0ff7d4d4aeb7c70e0eafb1f0d8e9049
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-05-20 19:00:08 -0300

    -Reformat color blend attachment code

--- eagle/renderer/vulkan/vulkan_shader.cpp
+++ eagle/renderer/vulkan/vulkan_shader.cpp
@@ -195,9 +195,10 @@ void VulkanShader::create_pipeline() {
             colorBlendAttachment[i].srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
             colorBlendAttachment[i].dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
             colorBlendAttachment[i].alphaBlendOp = VK_BLEND_OP_ADD;
-        }else{
-            colorBlendAttachment[i].colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        }
+        else {
             colorBlendAttachment[i].blendEnable = VK_FALSE;
+            colorBlendAttachment[i].colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
             colorBlendAttachment[i].srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
             colorBlendAttachment[i].dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
             colorBlendAttachment[i].colorBlendOp = VK_BLEND_OP_ADD; // Optional
@@ -205,8 +206,6 @@ void VulkanShader::create_pipeline() {
             colorBlendAttachment[i].dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
             colorBlendAttachment[i].alphaBlendOp = VK_BLEND_OP_ADD; // Optional
         }
-
-
     }
 
     VkPipelineDepthStencilStateCreateInfo depthStencil = {};
